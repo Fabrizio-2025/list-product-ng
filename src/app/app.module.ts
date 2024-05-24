a44eb2d989f51931269433f84b6d40beab49bf9f
Fabrizio-2025 <fabriziobc2020@outlook.com>
2024-05-24 01:36:59 -0500

    Feat: Add PrimeNg Style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// PrimeNG modules
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClassModule } from 'primeng/styleclass';
+
+// components
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductCreateComponent } from './components/product-create/product-create.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductCreateComponent,
-  ],
+  declarations: [AppComponent, ProductListComponent, ProductCreateComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,8 +26,14 @@ import { ProductCreateComponent } from './components/product-create/product-crea
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    // PrimeNG modules
+    SidebarModule,
+    ButtonModule,
+    RippleModule,
+    AvatarModule,
+    StyleClassModule,
   ],
-  providers: [provideAnimationsAsync()],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
